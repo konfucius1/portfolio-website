f0c625071ec20aff72ba2ca8ac6a70845dcadd2e
Thanakon Kimsan <72403268+konfucius1@users.noreply.github.com>
2023-03-02 02:23:55 +1100

    navbar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
-
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div class="bg-dark h-screen">
+      <nav class="bg-white border-gray-200 px-2 py-2.5 rounded dark:bg-gray-900">
+        <div class="container flex flex-wrap items-center justify-between mx-auto">
+          <div class="flex items-center">
+            <img src="" class="h-16 mr-3" alt="" />
+          </div>
+
+          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul class="flex flex-col items-center p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <div class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent font-light text-lg">
+                  about
+                </div>
+              </li>
+              <li>
+                <div class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent font-light text-lg">
+                  experience
+                </div>
+              </li>
+              <li>
+                <div class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent font-light text-lg">
+                  projects
+                </div>
+              </li>
+              <li>
+                <div class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent font-light text-lg">
+                  contact
+                </div>
+              </li>
+              <li>
+                <button class="font-montserrat btn text-white aspect-auto w-[140px] h-[50px] text-l">
+                  Resume
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      primary: '#2040CA',
+      secondary: '#4704AF',
+      accent: '#FF5E63',
+      dark: '#0D0C12',
+      'off-white': '#FFFCF5',
+    },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
